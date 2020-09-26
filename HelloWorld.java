b6d9acf198c1a0ab9bf9f1de57ae6b58f01987e9
st920202 <st920202@gmail.com>
2020-09-26 15:13:59 +0800

    second

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
+    	public float a;
         System.out.println("Hello World!, ©Ô©Ô©Ô...");
         System.out.println("Hello World!, ©Ô©Ô©Ô...");
     }
