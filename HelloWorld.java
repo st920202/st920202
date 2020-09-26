78dd59dafa25a36fc2db93643a81eb2ebf7fe049
st920202 <st920202@gmail.com>
2020-09-26 15:21:47 +0800

    hotfix2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
     	public float a;
-=======
-    	public int a;
->>>>>>> hotfix
+
         System.out.println("Hello World!, ©Ô©Ô©Ô...");
         System.out.println("Hello World!, ©Ô©Ô©Ô...");
     }
